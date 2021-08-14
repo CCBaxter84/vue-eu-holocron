55011b441c263d341886ba8ed53aac107044fbd2
CCBaxter84 <troyceclark@gmail.com>
2021-08-14 08:53:45 -0600

    Added BookGrid component

---
+++ src/components/BookGrid.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="book-grid" :class="hasLargeStyling">
+    <router-link v-for="book in books" :key="book.id" :to="'books/' + book.id">
+      <img :src="book.coverImageUrl" alt="" class="book-cover" />
+    </router-link>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BookGrid",
+  props: {
+    books: {
+      type: Array,
+      default: () => [],
+    },
+    isLargeGrid: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    hasLargeStyling(): string {
+      return this.isLargeGrid ? "book-grid-large" : "";
+    },
+  },
+});
+</script>
+
+<style scoped>
+.book-grid {
+  display: grid;
+  justify-content: center;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, var(--book-cover-width));
+}
+
+.book-grid-large {
+  grid-template-columns: repeat(auto-fill, var(--book-cover-width-large));
+}
+</style>
---
+++ src/views/Books.vue
@@ -0,0 +1,66 @@
+<template>
+  <main>
+    <h1 class="main-title">Search Books</h1>
+    <form action="/books" method="GET">
+      <section class="label-n-input">
+        <label>Title</label>
+        <input
+          autofocus
+          type="text"
+          class="text-box"
+          name="title"
+          value="{{"
+          searchOptions.title
+          }}
+        />
+      </section>
+      <section class="split-inputs">
+        <section class="label-n-input">
+          <label>Keywords</label>
+          <input
+            type="text"
+            class="text-box"
+            name="keywords"
+            value="{{ searchOptions.keywords }}"
+          />
+        </section>
+        <!-- {{> selectBookEra eras=eras book=book searchOptions=searchOptions}} -->
+      </section>
+      <section class="split-inputs">
+        <section class="label-n-input">
+          <label>Published After</label>
+          <input
+            type="date"
+            class="text-box"
+            name="publishedAfter"
+            value="{{ searchOptions.publishedAfter }}"
+          />
+        </section>
+        <section class="label-n-input">
+          <label>Published Before</label>
+          <input
+            type="date"
+            class="text-box"
+            name="publishedBefore"
+            value="{{ searchOptions.publishedBefore }}"
+          />
+        </section>
+      </section>
+      <!-- {{> submitSearch }} -->
+    </form>
+    <br />
+    <BookGrid></BookGrid>
+    <!-- {{> bookGrid books=books }} -->
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BookGrid from "/components/BookGrid.vue";
+export default defineComponent({
+  name: "App",
+  components: { BookGrid },
+});
+</script>
+
+<style></style>
